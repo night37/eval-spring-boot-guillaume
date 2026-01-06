ed00c011f1f965c47db35a185b36b11ea5d84297
guillaume <garnier.guillaume37550@gmail.com>
2026-01-06 14:12:51 +0100

    first commit

---
+++ evalspring/src/test/java/com/adrar/evalspring/EvalspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrar.evalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
